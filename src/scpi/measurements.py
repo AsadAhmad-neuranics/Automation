e0cfe76f232492c227c4d5aa619829fc44ffc473
Asad Ahmad <aa4523@ic.ac.uk>
2025-06-30 12:10:21 +0100

    Added the I_B class to measure input bias current and modified the V_os class to include a temperature chamber so that a constant temperature of 25 celsius is the default for all measurements.

--- src/scpi/measurements.py
+++ src/scpi/measurements.py
@@ -16,10 +16,12 @@ class V_os:
     close():
         Closes the power supply and oscilloscope connections.
     """
-    def __init__(self, gain, ps_address='GPIB0::5::INSTR', osc_address='SPIB0::7::INSTR'):
+    def __init__(self, gain, ps_address='GPIB0::5::INSTR', osc_address='SPIB0::7::INSTR', chamber_address='GPIB0::12::INSTR'):
         self.gain = gain
         self.ps = PowerSupply(address=ps_address)
         self.osc = Oscilloscope(address=osc_address)
+        self.tc = TemperatureChamber(address=chamber_address)
+        self.tc.set_temperature(25)  # Set initial temperature
 
     def measure(self, v_in=0.0, channel=1, n_points=1000, sample_rate=1000):
         # Set input voltage to v_in
@@ -39,6 +41,7 @@ class V_os:
     def close(self):
         self.ps.close()
         self.osc.close()
+        self.tc.close()
 
 class V_os_drift(V_os):
     """
@@ -52,8 +55,7 @@ class V_os_drift(V_os):
         Closes the power supply, oscilloscope, and temperature chamber connections.
     """
     def __init__(self, gain, ps_address='GPIB0::5::INSTR', osc_address='SPIB0::7::INSTR', chamber_address='GPIB0::12::INSTR'):
-        super().__init__(gain, ps_address, osc_address)
-        self.tc = TemperatureChamber(address=chamber_address)
+        super().__init__(gain, ps_address, osc_address, chamber_address)
 
     def measure_drift(self, temp_list, v_in=0.0, channel=1, n_points=1000, sample_rate=1000, settle_time=120):
         """
@@ -82,5 +84,58 @@ class V_os_drift(V_os):
 
     def close(self):
         super().close()
+
+
+class I_B:
+    """
+    Automates the measurement of input bias current (I_B) for an op-amp by measuring the voltage drop across a high-value resistor in series with the input.
+
+    Parameters
+    ----------
+    gain : float
+        The gain of the amplifier circuit.
+    ps_address : str
+        VISA address of the power supply.
+    osc_address : str
+        VISA address of the oscilloscope.
+    res : float
+        Resistance value in ohms (default: 10000).
+
+    Methods
+    -------
+    measure_ib(v_in=0.0, channel=1, n_points=1000, sample_rate=1000):
+        Sets the input voltage, measures the voltage drop across the resistor, and calculates the input bias current.
+    close():
+        Closes the power supply and oscilloscope connections.
+    """
+    def __init__(self, gain, ps_address='GPIB0::5::INSTR', osc_address='SPIB0::7::INSTR', chamber_address='GPIB0::12::INSTR',res: float = 10000, temp=25):
+        self.gain = gain
+        self.ps = PowerSupply(address=ps_address)
+        self.osc = Oscilloscope(address=osc_address)
+        self.tc = TemperatureChamber(address=chamber_address)
+        self.tc.set_temperature(temp)  # Set initial temperature
+        self.temperatre = self.tc.get_temperature()
+        self.resistance = res  # Resistance in ohms
+
+    def measure_ib(self, v_in=0.0, channel=1, n_points=1000, sample_rate=1000):
+        """
+        Measures the input bias current by applying v_in, measuring the voltage drop across the resistor,
+        and calculating I_B = V_drop / R.
+        """
+        self.ps.ps.write(f'VOLT {v_in}')
+        self.ps.ps.write('OUTP ON')
+        time.sleep(1)  # Wait for stabilization
+        timebase, voltages = self.osc.waveform(channel=channel, n_points=n_points, sample_rate=sample_rate)
+        v_typ = np.mean(voltages) / self.gain  # Average voltage drop across resistor
+        v_max = np.amax(voltages) / self.gain
+        ib_typ = v_typ / self.resistance  # Input bias current in Amps
+        ib_max = v_max / self.resistance
+        self.ps.ps.write('OUTP OFF')
+
+        return ib_typ, ib_max
+
+    def close(self):
+        self.ps.close()
+        self.osc.close()
         self.tc.close()
 
